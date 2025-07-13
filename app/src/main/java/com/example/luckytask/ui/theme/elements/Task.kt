3a5264d5db8c9dc51e0aeee064e2ad10a4e063f5
Selina Isak <seisak@edu.aau.at>
2025-07-14 01:34:43 +0200

    Add info button to task

--- app/src/main/java/com/example/luckytask/ui/theme/elements/Task.kt
+++ app/src/main/java/com/example/luckytask/ui/theme/elements/Task.kt
@@ -3,11 +3,14 @@ package com.example.luckytask.ui.theme.elements
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.luckytask.R
@@ -31,5 +34,12 @@ fun Task(
             modifier = Modifier
                 .padding(horizontal = 16.dp, vertical = 12.dp)
         )
+        IconButton(onClick = {}) {
+            Icon(
+                painter = painterResource(R.drawable.info),
+                contentDescription = "Info",
+                tint = colorResource(R.color.task_color)
+            )
+        }
     }
 }
