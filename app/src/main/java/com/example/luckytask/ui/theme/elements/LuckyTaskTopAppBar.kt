7cf671a9ec8e97001614349e9e326202dcc707ef
Selina Isak <seisak@edu.aau.at>
2025-07-13 22:49:52 +0200

    Create a template for a custom top app bar

---
+++ app/src/main/java/com/example/luckytask/ui/theme/elements/LuckyTaskTopAppBar.kt
@@ -0,0 +1,11 @@
+package com.example.luckytask.ui.theme.elements
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun LuckyTaskTopAppBar(
+    /*** Use the title for 'naming' the top bar ***/
+    title: String
+) {
+
+}
