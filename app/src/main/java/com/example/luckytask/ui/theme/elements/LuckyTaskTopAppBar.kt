d40a1a089146f17e4c664a062a2dbddfd4cba438
Selina Isak <seisak@edu.aau.at>
2025-07-13 23:46:23 +0200

    Make font of top app bar bigger

--- app/src/main/java/com/example/luckytask/ui/theme/elements/LuckyTaskTopAppBar.kt
+++ app/src/main/java/com/example/luckytask/ui/theme/elements/LuckyTaskTopAppBar.kt
@@ -1,12 +1,19 @@
 package com.example.luckytask.ui.theme.elements
 
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.unit.sp
 import com.example.luckytask.R
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -14,10 +21,13 @@ import com.example.luckytask.R
 fun LuckyTaskTopAppBar(
     /*** Use the title for 'naming' the top bar ***/
     title: String,
-    color: Color = colorResource(R.color.lilac)
+    color: Color = colorResource(R.color.lilac),
 ) {
     TopAppBar(
-        title = { Text(title) },
+        title = { Text(
+            text = title,
+            fontSize = 35.sp
+        ) },
         colors = TopAppBarColors(
             containerColor = color,
             scrolledContainerColor = color,
