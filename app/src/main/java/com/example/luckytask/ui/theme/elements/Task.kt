aac92dd22deec10897a7bf8b73e10ac67e38cb6f
Selina Isak <seisak@edu.aau.at>
2025-07-14 01:41:00 +0200

    Fix alignment in task using Row + add comments

--- app/src/main/java/com/example/luckytask/ui/theme/elements/Task.kt
+++ app/src/main/java/com/example/luckytask/ui/theme/elements/Task.kt
@@ -1,5 +1,7 @@
 package com.example.luckytask.ui.theme.elements
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -8,6 +10,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
@@ -18,7 +21,7 @@ import com.example.luckytask.R
 @Composable
 fun Task(
     title: String,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
 ) {
     Surface(
         modifier = modifier
@@ -27,19 +30,29 @@ fun Task(
         color = colorResource(R.color.app_color),
         shape = RoundedCornerShape(10.dp)
     ) {
-        Text(
-            text = title,
-            fontSize = 25.sp,
-            color = colorResource(R.color.task_color),
+        /*** Align all elements in a row, to organize them horizontally ***/
+        Row(
             modifier = Modifier
-                .padding(horizontal = 16.dp, vertical = 12.dp)
-        )
-        IconButton(onClick = {}) {
-            Icon(
-                painter = painterResource(R.drawable.info),
-                contentDescription = "Info",
-                tint = colorResource(R.color.task_color)
+                .fillMaxWidth()
+                /*** Add some padding, so the text is not that close to the border ***/
+                .padding(horizontal = 15.dp, vertical = 10.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = title,
+                fontSize = 25.sp,
+                color = colorResource(R.color.task_color),
+                modifier = Modifier
+                    .padding(horizontal = 16.dp, vertical = 12.dp)
             )
+            IconButton(onClick = {}) {
+                Icon(
+                    painter = painterResource(R.drawable.info),
+                    contentDescription = "Info",
+                    tint = colorResource(R.color.task_color)
+                )
+            }
         }
     }
 }
