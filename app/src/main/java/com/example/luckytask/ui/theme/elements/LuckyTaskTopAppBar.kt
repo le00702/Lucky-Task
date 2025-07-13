bec6a1a9b97ce084afae26d6092a3585338fd79d
Selina Isak <seisak@edu.aau.at>
2025-07-14 00:34:35 +0200

    Add hamburger menu to left side of top bar text

--- app/src/main/java/com/example/luckytask/ui/theme/elements/LuckyTaskTopAppBar.kt
+++ app/src/main/java/com/example/luckytask/ui/theme/elements/LuckyTaskTopAppBar.kt
@@ -1,8 +1,13 @@
 package com.example.luckytask.ui.theme.elements
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarColors
@@ -11,6 +16,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.sp
 import com.example.luckytask.R
 
@@ -35,6 +41,15 @@ fun LuckyTaskTopAppBar(
                 Text(
                     text = title,
                     fontSize = 35.sp
+                )
+            }
+        },
+        navigationIcon = {
+            IconButton(onClick = {}) {
+                Icon(
+                    painter = painterResource(R.drawable.hamburger_menu),
+                    contentDescription = "Menu",
+                    tint = contentColor
                 )
             }
         },
