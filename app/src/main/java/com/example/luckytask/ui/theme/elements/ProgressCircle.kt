6b0a8b92896bfb4d500ce71d5edf7f433cad145d
Selina Isak <seisak@edu.aau.at>
2025-07-14 03:10:15 +0200

    Refine circular progress bar + add comments

--- app/src/main/java/com/example/luckytask/ui/theme/elements/ProgressCircle.kt
+++ app/src/main/java/com/example/luckytask/ui/theme/elements/ProgressCircle.kt
@@ -5,6 +5,7 @@ import androidx. compose. ui. graphics. Color
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -13,7 +14,15 @@ fun ProgressCircle(done: Int, total: Int, color: Color) {
         progress = {
             (done.toFloat() / total.toFloat())
         },
+        /*** Size of the progress indicator ***/
         modifier = Modifier.size(200.dp),
-        color = color
+        /*** Color of the progress indicator***/
+        color = color,
+        /*** The width of the progress indicator + track ***/
+        strokeWidth = 20.dp,
+        /*** This controls how big the gap between track and progress indicator is ****/
+        gapSize = 0.dp,
+        /*** This controls the shape of the progress indicator ends (round, square, etc.) ***/
+        strokeCap = StrokeCap.Square
     )
 }
