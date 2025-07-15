590082719cfb33294e257c672f3741553c944550
Selina Isak <seisak@edu.aau.at>
2025-07-15 23:27:23 +0200

    Init shake event listener

---
+++ app/src/main/java/com/example/luckytask/sensor/ShakeListener.kt
@@ -0,0 +1,16 @@
+package com.example.luckytask.sensor
+
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+
+class ShakeListener(): SensorEventListener {
+    override fun onSensorChanged(event: SensorEvent?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        TODO("Not yet implemented")
+    }
+
+}
