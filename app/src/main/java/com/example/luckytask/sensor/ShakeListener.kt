d8b74769b8086288addea04a3d7126b23730c464
Selina Isak <seisak@edu.aau.at>
2025-07-16 20:28:15 +0200

    Add preliminary methods + refactoring

--- app/src/main/java/com/example/luckytask/sensor/ShakeListener.kt
+++ app/src/main/java/com/example/luckytask/sensor/ShakeListener.kt
@@ -5,23 +5,35 @@ import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.util.Log
 
 /*** Add constructor taking context and onShake function --> extend SensorEventListener
  * --> to avoid it being a separate Activity ****/
 class ShakeListener(
-    private val context: Context,
-    private val onShake: () -> Unit,
+    context: Context,
+    onShake: () -> Unit,
 ) : SensorEventListener {
     private val sensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager?
     private val accelerometer = sensorManager?.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
 
+    fun start() {
+        sensorManager?.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL)
+        Log.d("[SENSOR]", "Registered sensor")
+    }
+
+    fun stop() {
+        sensorManager?.unregisterListener(this)
+        Log.d("[SENSOR]", "Unregistered sensor")
+    }
 
     override fun onSensorChanged(event: SensorEvent?) {
-        TODO("Not yet implemented")
+        /*** Change later --> TODOs result in crash ***/
+        Log.d("[SENSOR]", "Sensor ${event?.sensor?.name} changed")
     }
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-        TODO("Not yet implemented")
+        /*** Change later maybe --> TODOs result in crash ***/
+        Log.d("[SENSOR]", "Accuracy of sensor ${sensor?.name} changed to $accuracy")
     }
 
 }
