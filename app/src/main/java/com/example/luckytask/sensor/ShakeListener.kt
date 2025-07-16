db5e8a83bb8b83fef81d35bfad0ec1d138fdf9e9
Selina Isak <seisak@edu.aau.at>
2025-07-17 00:00:50 +0200

    Implement first attempt at shake detection --> DONE ON OLD PHYSICAL DEVICE

--- app/src/main/java/com/example/luckytask/sensor/ShakeListener.kt
+++ app/src/main/java/com/example/luckytask/sensor/ShakeListener.kt
@@ -6,15 +6,22 @@ import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.util.Log
+import kotlin.math.sqrt
 
 /*** Add constructor taking context and onShake function --> extend SensorEventListener
  * --> to avoid it being a separate Activity ****/
 class ShakeListener(
     context: Context,
-    onShake: () -> Unit,
+    private val onShake: () -> Unit,
 ) : SensorEventListener {
     private val sensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager?
     private val accelerometer = sensorManager?.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+    
+    /*** The value of 15 was chosen based on experiments on a physical device
+     *   --> emulator did not work for this. We started at 2, got to 5, 10, and then went
+     *   up to 15. 15 provides a strong protection against recognizing random/slower movements
+     *   as a "shaking motion" ***/
+    private val threshold = 15
 
     fun start() {
         sensorManager?.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL)
@@ -26,10 +33,23 @@ class ShakeListener(
         Log.d("[SENSOR]", "Unregistered sensor")
     }
 
-    override fun onSensorChanged(event: SensorEvent?) {
-        /*** Change later --> TODOs result in crash ***/
-        Log.d("[SENSOR]", "Sensor ${event?.sensor?.name} changed")
+    /*** Calculate acceleration for shake detection --> used this StackOverflow post
+     *   for help: https://stackoverflow.com/questions/79414515/detect-shake-on-android-in-jetpack-compose ****/
+    override fun onSensorChanged(event: SensorEvent) {
+        val x = event.values[0]
+        val y = event.values[1]
+        val z = event.values[2]
+
+        /*** Removing the gravitational force of the earth is necessary as it influences
+         *   the values --> around 9.81 for z (straight down, when laying flat) ***/
+        val acceleration = sqrt(x * x + y * y + z * z) - SensorManager.GRAVITY_EARTH
+        /*Log.d("[SENSOR]", "x: $x, y: $y, z: $z")*/
+        if (acceleration > threshold) {
+            Log.d("[SENSOR]", "Acc: $acceleration, Thr: $threshold")
+            onShake()
+        }
     }
+
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
         /*** Change later maybe --> TODOs result in crash ***/
