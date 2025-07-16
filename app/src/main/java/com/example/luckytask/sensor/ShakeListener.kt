5c2bed7da175535ef52ee22c68681c13d04bc7fb
Selina Isak <seisak@edu.aau.at>
2025-07-16 19:58:56 +0200

    Implement constructor for Shake Detection + sensor management

--- app/src/main/java/com/example/luckytask/sensor/ShakeListener.kt
+++ app/src/main/java/com/example/luckytask/sensor/ShakeListener.kt
@@ -1,10 +1,21 @@
 package com.example.luckytask.sensor
 
+import android.content.Context
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 
-class ShakeListener(): SensorEventListener {
+/*** Add constructor taking context and onShake function --> extend SensorEventListener
+ * --> to avoid it being a separate Activity ****/
+class ShakeListener(
+    private val context: Context,
+    private val onShake: () -> Unit,
+) : SensorEventListener {
+    private val sensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager?
+    private val accelerometer = sensorManager?.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+
+
     override fun onSensorChanged(event: SensorEvent?) {
         TODO("Not yet implemented")
     }
