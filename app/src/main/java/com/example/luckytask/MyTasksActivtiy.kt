e9271975f0bf3aef6ddb237ea050493e721f416d
Selina Isak <seisak@edu.aau.at>
2025-07-19 01:14:29 +0200

    Differentiate whether tasks are active or not --> if not, display message for rolling dice

--- app/src/main/java/com/example/luckytask/MyTasksActivtiy.kt
+++ app/src/main/java/com/example/luckytask/MyTasksActivtiy.kt
@@ -15,7 +15,9 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.luckytask.sensor.ShakeListener
@@ -74,12 +76,19 @@ fun TasksScreen(modifier: Modifier = Modifier, triggerAnimation: MutableState<Bo
 
     val HEADER_SIZE = 30.sp
 
+    /*** Use this active-task-list for mocking purposes for now ***/
+    var activeTasks = listOf<String>()
+
+    /*** ENABLE WHEN CHECKING FOR ACTIVE TASKS DISPLAY ***/
+    activeTasks = listOf<String>("Task 1", "Task 2", "Task 3")
+
     /*** Organize elements in column ***/
     LazyColumn(
         modifier = modifier.padding(20.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
     )
     {
+
         item {
             Text(
                 text = "My Active Tasks",
@@ -87,12 +96,27 @@ fun TasksScreen(modifier: Modifier = Modifier, triggerAnimation: MutableState<Bo
             )
         }
 
-        item {
-            Task(
-                "This is a task",
-                modifier = Modifier,
-                active = true
-            )
+        /*** If there are no active tasks, display the following message
+         *   --> user is asked to roll dice
+         *   --> align text centered ***/
+        if(activeTasks.isEmpty()) {
+            item {
+                Text(
+                    "You currently have no active tasks. Roll the dice to start a task!",
+                    color = colorResource(R.color.task_text_color),
+                    fontSize = 20.sp,
+                    textAlign = TextAlign.Center
+                )
+            }
+        } else {
+            /*** If there ARE active tasks, display them all ***/
+            items(activeTasks.size) { index ->
+                Task(
+                    title = activeTasks[index],
+                    modifier = Modifier,
+                    active = true
+                )
+            }
         }
 
         item {
