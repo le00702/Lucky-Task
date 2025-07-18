cf1b8b4fbb5dcd77196f1c2ce92e877c8736a9b8
Selina Isak <seisak@edu.aau.at>
2025-07-18 22:39:34 +0200

    Integrate dice into task screen

--- app/src/main/java/com/example/luckytask/TasksActivtiy.kt
+++ app/src/main/java/com/example/luckytask/TasksActivtiy.kt
@@ -1,6 +1,7 @@
 package com.example.luckytask
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -10,21 +11,40 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.luckytask.sensor.ShakeListener
 import com.example.luckytask.ui.theme.LuckyTaskTheme
 import com.example.luckytask.ui.theme.elements.AddTaskButton
+import com.example.luckytask.ui.theme.elements.Dice
 import com.example.luckytask.ui.theme.elements.LuckyTaskTopAppBar
 import com.example.luckytask.ui.theme.elements.ProgressCircle
 import com.example.luckytask.ui.theme.elements.Task
 
 class TasksActivity : ComponentActivity() {
+    private lateinit var shakeListener: ShakeListener
+    private val TAG = "[SENSOR]"
+
+    /*** Use this variable to keep track of animation ***/
+    private var triggerAnimation = mutableStateOf(false)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        /*** Pass the code for onShake() for shakeListener --> trigger animation ***/
+        shakeListener = ShakeListener(this) {
+            Log.d(TAG, "Shake detected!")
+            triggerAnimation.value = true
+        }
+
         enableEdgeToEdge()
         setContent {
             LuckyTaskTheme {
@@ -34,16 +54,27 @@ class TasksActivity : ComponentActivity() {
                     )
                 }) { innerPadding ->
                     TasksScreen(
-                        modifier = Modifier.padding(innerPadding)
+                        modifier = Modifier.padding(innerPadding),
+                        triggerAnimation
                     )
                 }
             }
         }
     }
+
+    override fun onResume() {
+        super.onResume()
+        shakeListener.start()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        shakeListener.stop()
+    }
 }
 
 @Composable
-fun TasksScreen(modifier: Modifier = Modifier) {
+fun TasksScreen(modifier: Modifier = Modifier, triggerAnimation: MutableState<Boolean>) {
 
     /*** Organize elements in column ***/
     Column(
@@ -52,9 +83,20 @@ fun TasksScreen(modifier: Modifier = Modifier) {
         verticalArrangement = Arrangement.Center
     )
     {
+        Text(
+            text = "My Active Tasks",
+            fontSize = 30.sp
+        )
+
         Task(
             "This is a task",
             modifier = Modifier
+        )
+        Dice(
+            modifier = modifier,
+
+            /*** Pass value of triggerAnimation to Dice for the actual animation ***/
+            triggerAnimation = triggerAnimation
         )
         AddTaskButton(
             modifier = Modifier
