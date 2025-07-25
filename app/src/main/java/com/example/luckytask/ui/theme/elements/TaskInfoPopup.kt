b96c7f06cd98fbdea162d7023dfba6a6e077e994
Selina Isak <seisak@edu.aau.at>
2025-07-26 01:35:45 +0200

    Add the info icon on top in the popup

--- app/src/main/java/com/example/luckytask/ui/theme/elements/TaskInfoPopup.kt
+++ app/src/main/java/com/example/luckytask/ui/theme/elements/TaskInfoPopup.kt
@@ -5,13 +5,16 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Popup
@@ -28,11 +31,21 @@ fun TaskInfoPopup(title: String, text: String, onDismissRequest: () -> Unit) {
         onDismissRequest = onDismissRequest,
         alignment = Alignment.Center
     ) {
+        /*** Display info icon, task title, task description in a column ***/
         Column (
             modifier = Modifier
                 .background(Color.White, RoundedCornerShape(12.dp))
                 .padding(horizontal = 16.dp, vertical = 12.dp)
         ) {
+            /*** Add the info icon on top but much smaller and center it ***/
+            Icon(
+                painter = painterResource(R.drawable.info),
+                contentDescription = "Info",
+                tint = colorResource(R.color.task_text_color),
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .size(20.dp)
+            )
             Text(
                 text = title,
                 fontSize = 20.sp,
