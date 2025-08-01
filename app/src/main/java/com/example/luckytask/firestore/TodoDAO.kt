35356d8c8abdb08cce8d3a3a015697485ba0847b
Leonardo <leonardotocci6@gmail.com>
2025-08-01 02:32:46 +0200

    Added default values to all values of TodoDAO

--- app/src/main/java/com/example/luckytask/firestore/TodoDAO.kt
+++ app/src/main/java/com/example/luckytask/firestore/TodoDAO.kt
@@ -1,3 +1,6 @@
 package com.example.luckytask.firestore
 
-data class TodoDAO(var title:String, var text:String, var done:Boolean = false)
+data class TodoDAO(var title:String = "",
+                   var text:String = "",
+                   var done:Boolean = false,
+                   var id:String? = null)
