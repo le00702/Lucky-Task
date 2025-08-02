51b03a7bcc3729f317b2e253aed1d3e1f5810b0d
Leonardo <leonardotocci6@gmail.com>
2025-08-02 02:06:43 +0200

    modified GroupDAO for Firebase use

--- app/src/main/java/com/example/luckytask/firestore/GroupDAO.kt
+++ app/src/main/java/com/example/luckytask/firestore/GroupDAO.kt
@@ -1,3 +1,8 @@
 package com.example.luckytask.firestore
 
-data class GroupDAO(val users:List<UserDAO>, val tasks:List<TodoDAO>)
+data class GroupDAO(
+    var id:String? = null, //Set by Firestore
+    var name:String = "Group",
+    var joinKey:String = "12345", //Set when creating Group
+    //UserList and TaskList: implemented in Firestore as Collection inside Group document
+)
