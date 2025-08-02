00dfea0e4a5035986a510c9720c4866468b7ec8c
Leonardo <leonardotocci6@gmail.com>
2025-08-02 02:07:44 +0200

    modified UserDAO for use in Firebase

--- app/src/main/java/com/example/luckytask/firestore/UserDAO.kt
+++ app/src/main/java/com/example/luckytask/firestore/UserDAO.kt
@@ -1,3 +1,7 @@
 package com.example.luckytask.firestore
 
-data class UserDAO(val name:String, val id:String)
+data class UserDAO(
+    var id:String? = null,
+    var name:String = "User",
+    var nickName:String? = "U",//short (one, two letter nickname)
+)
