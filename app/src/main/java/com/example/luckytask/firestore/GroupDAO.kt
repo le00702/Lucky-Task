759d2cdb1587df7760b953599a3ac90a758edfff
Leonardo <leonardotocci6@gmail.com>
2025-08-06 12:55:50 +0200

    changed order of arguments allows for simpler instantiation

--- app/src/main/java/com/example/luckytask/firestore/GroupDAO.kt
+++ app/src/main/java/com/example/luckytask/firestore/GroupDAO.kt
@@ -1,8 +1,8 @@
 package com.example.luckytask.firestore
 
 data class GroupDAO(
-    var id:String? = null, //Set by Firestore
     var name:String = "Group",
+    var id:String? = null, //Set by Firestore
     var joinKey:String = "12345", //Set when creating Group
     //UserList and TaskList: implemented in Firestore as Collection inside Group document
 )
