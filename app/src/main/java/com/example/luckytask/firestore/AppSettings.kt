7da17672a4e7988a578e762f54a96f1c104d486f
Leonardo <leonardotocci6@gmail.com>
2025-08-06 19:48:21 +0200

    rewrote group related function to store Mapped objects for easier access and duplicate checking

--- app/src/main/java/com/example/luckytask/firestore/AppSettings.kt
+++ app/src/main/java/com/example/luckytask/firestore/AppSettings.kt
@@ -20,59 +20,65 @@ import kotlinx.serialization.json.Json
 val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 
 class AppSettings {
-    companion object{
+    companion object {
         val USER_KEY = stringPreferencesKey("user_key")
         val GROUP_LIST_KEY = stringPreferencesKey("group_key")
-    }
 
-    suspend fun getUserInfo(context: Context):UserDAO?{
-        val data = context.dataStore.data.first()
-        val json = data[USER_KEY]?: return null
-        return Json.decodeFromString<UserDAO>(json)
-    }
-
-    suspend fun setUserInfo(context: Context, user: UserDAO) {
-        val json = Json.encodeToString(user)
-        context.dataStore.edit { settings ->
-            settings[USER_KEY] = json
+        suspend fun getUserInfo(context: Context): UserDAO? {
+            val data = context.dataStore.data.first()
+            val json = data[USER_KEY] ?: return null
+            return Json.decodeFromString<UserDAO>(json)
         }
-    }
 
-    suspend fun getGroups(context: Context): MutableSet<GroupDAO>?{
-        val data = context.dataStore.data.first()
-        val json = data[GROUP_LIST_KEY]?: return null
-        return Json.decodeFromString<MutableSet<GroupDAO>>(json)
-    }
-
-    suspend fun setGroups(context: Context, groups:MutableSet<GroupDAO>){
-        val json = Json.encodeToString(groups)
-        context.dataStore.edit { settings ->
-            settings[GROUP_LIST_KEY] = json
+        suspend fun setUserInfo(context: Context, user: UserDAO) {
+            val json = Json.encodeToString(user)
+            context.dataStore.edit { settings ->
+                settings[USER_KEY] = json
+            }
         }
-    }
 
-    suspend fun addGroup(context: Context, group:GroupDAO){
-        val data = context.dataStore.data.first()
-        val json = data[GROUP_LIST_KEY]
-        if(json == null){
-            Log.i("AppSettings", "Adding first group")
-            setGroups(context, mutableSetOf(group))
-            return
+        suspend fun getGroups(context: Context): MutableMap<String,String>? {
+            val data = context.dataStore.data.first()
+            val json = data[GROUP_LIST_KEY] ?: return null
+            return Json.decodeFromString<MutableMap<String,String>>(json)
         }
-        val list = Json.decodeFromString<MutableSet<GroupDAO>>(json)
-        list.add(group)
-        setGroups(context, list)
-    }
 
-    suspend fun removeGroup(context: Context, group:GroupDAO){
-        val data = context.dataStore.data.first()
-        val json = data[GROUP_LIST_KEY]
-        if(json == null){
-            Log.i("AppSettings", "No Group Found when trying to remove")
-            return
+        suspend fun setGroups(context: Context, groups: MutableMap<String,String>) {
+            val json = Json.encodeToString(groups)
+            context.dataStore.edit { settings ->
+                settings[GROUP_LIST_KEY] = json
+            }
         }
-        val list = Json.decodeFromString<MutableSet<GroupDAO>>(json)
-        list.remove(group)
-        setGroups(context, list)
+
+        suspend fun addGroup(context: Context, group: GroupDAO) {
+            val data = context.dataStore.data.first()
+            val json = data[GROUP_LIST_KEY]
+            if (json == null) {
+                Log.i("AppSettings", "Adding first group")
+                setGroups(context, mutableMapOf(group.id to group.name))
+                return
+            }
+            val list = Json.decodeFromString<MutableMap<String,String>>(json)
+            if(list.containsKey(group.id)){
+                Log.i("AppSettings", "Group already exists")
+                return
+            }else{
+                list.put(group.id, group.name)
+            }
+            Log.i("AppSettings", "Adding ${group.name} to groups")
+            setGroups(context, list)
+        }
+
+        suspend fun removeGroup(context: Context, group: GroupDAO) {
+            val data = context.dataStore.data.first()
+            val json = data[GROUP_LIST_KEY]
+            if (json == null) {
+                Log.i("AppSettings", "No Group Found when trying to remove")
+                return
+            }
+            val list = Json.decodeFromString<MutableMap<String, String>>(json)
+            list.remove(group.id)
+            setGroups(context, list)
+        }
     }
 }
