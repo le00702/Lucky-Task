19804a62baaf88e14ce0d77eb773717f70b4d079
Leonardo <leonardotocci6@gmail.com>
2025-08-06 19:33:14 +0200

    Changed GroupDAO id: key set by user when creating Group used as ID in Firestore and by user to join group

--- app/src/main/java/com/example/luckytask/firestore/GroupDAO.kt
+++ app/src/main/java/com/example/luckytask/firestore/GroupDAO.kt
@@ -5,7 +5,6 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class GroupDAO(
     var name:String = "Group",
-    var id:String? = null, //Set by Firestore
-    var joinKey:String = "12345", //Set when creating Group
+    var id:String = "12345678", //Set by user when creating Group
     //UserList and TaskList: implemented in Firestore as Collection inside Group document
 )
