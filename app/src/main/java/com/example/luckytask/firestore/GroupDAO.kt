002c5b4a2d254295dbf6740b6240659d40892021
Leonardo <leonardotocci6@gmail.com>
2025-08-07 15:12:24 +0200

    swap order of elements

--- app/src/main/java/com/example/luckytask/firestore/GroupDAO.kt
+++ app/src/main/java/com/example/luckytask/firestore/GroupDAO.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class GroupDAO(
-    var name:String = "Group",
     var id:String = "12345678", //Set by user when creating Group
+    var name:String = "Group"
     //UserList and TaskList: implemented in Firestore as Collection inside Group document
 )
